bc813b8753072e45a70965fb70126f1f2b36f138
Chuck M <you@example.com>
2020-03-31 20:46:31 +0300

    initial main added

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+//servLocator.h
+//
+char * getSignalFromServer(char *message);
+
+char * setSignalToServer  (char *message);
+
+int main(const char * args[])
+{
+    std::cin >> setSignalToServer;
+    std::cout << getSignalFromServer;
+    return 0;
+
+    //перебор значений аргументов командной строки отп польз. при запуске программы
+    
+    //for цикл 
+    for (int i;i<LEN(args) args++)
+    {
+        switch ()
+        {
+        case args[i]=="-v"/* constant-expression */:
+            /* code */
+            break;
+        
+        default:
+            cout<<"error";
+            break;
+        }
+        case 
+
+    } 
+    //foreach
+    for (char * arg : args) 
+    {
+
+    }
+}
+
+//servLocator.cpp
+char * getSignalFromServer(char *message)
+{
+
+}
+
+char * setSignalToServer  (char *message)
+{
+
+}
